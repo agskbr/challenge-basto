89b932cf674963db8ef62d2a3b8c690fc87b5332
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-08 16:39:34 -0300

    creating server model with db connection, configuration routes and middlewares

---
+++ challenge-basto-back/src/models/server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dbConnection = require("../database/configDB");
+
+function Server() {
+  this.app = express();
+  this.port = process.env.PORT;
+  this.paths = {};
+
+  //Config db
+  this.connectToDB();
+
+  //config middlewares
+  this.configMiddlewares();
+
+  //config routes
+  this.configRoutes();
+}
+
+Server.prototype.connectToDB = async function () {
+  await dbConnection();
+};
+
+Server.prototype.configRoutes = function () {
+  //   this.app.use(this.paths.);
+};
+
+Server.prototype.configMiddlewares = function () {
+  this.app.use(cors());
+  this.app.use(express.json());
+  this.app.use(morgan("dev"));
+};
+
+Server.prototype.listen = function () {
+  this.app.listen(this.port, () => {
+    console.log("Server running on port", this.port);
+  });
+};
+
+module.exports = Server;
