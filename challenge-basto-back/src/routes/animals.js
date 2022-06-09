5bd8173e5236b758467e507e51a638324a998781
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-09 08:24:35 -0300

    Configure router with get, put and delete routes

--- challenge-basto-back/src/routes/animals.js
+++ challenge-basto-back/src/routes/animals.js
@@ -1,8 +1,22 @@
 const { Router } = require("express");
-const { createAnimal } = require("../controllers/animals");
+const {
+  createAnimal,
+  getAllAnimals,
+  getAnimalById,
+  updateAnimal,
+  deleteAnimal,
+} = require("../controllers/animals");
 
 const router = Router();
 
 router.post("/", createAnimal);
+
+router.get("/", getAllAnimals);
+
+router.get("/:id", getAnimalById);
+
+router.put("/:id", updateAnimal);
+
+router.delete("/:id", deleteAnimal);
 
 module.exports = router;
