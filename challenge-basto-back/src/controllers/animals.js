8006ec514676b1baffae0612d10819c9ed64abf5
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-09 08:23:52 -0300

    Adding get, put and delete methods to controller

--- challenge-basto-back/src/controllers/animals.js
+++ challenge-basto-back/src/controllers/animals.js
@@ -17,13 +17,55 @@ const createAnimal = async (req, res) => {
   }
 };
 
-const getAllAnimals = async (req, res) => {};
+const getAllAnimals = async (req, res) => {
+  try {
+    const animals = await Animal.find({});
+    res.json(animals);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error });
+  }
+};
 
-const getAnimalById = async (req, res) => {};
+const getAnimalById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const animal = await Animal.findById(id);
+    res.json(animal);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error });
+  }
+};
 
-const updateAnimal = async (req, res) => {};
+const updateAnimal = async (req, res) => {
+  try {
+    const { senasaId, type, weight, deviceType, deviceNum } = req.body;
+    const { id } = req.params;
+    const updatedAnimal = await Animal.findByIdAndUpdate(id, {
+      ID_SENASA: senasaId,
+      type,
+      weight,
+      deviceType,
+      deviceNum,
+    });
+    res.json({ updated: true, updatedAnimal });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error });
+  }
+};
 
-const deleteAnimal = async (req, res) => {};
+const deleteAnimal = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedAnimal = await Animal.findByIdAndDelete(id);
+    res.json({ deleted: true, deletedAnimal });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error });
+  }
+};
 
 module.exports = {
   createAnimal,
