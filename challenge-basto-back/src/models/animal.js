48e4aaad739f93284b7069dd5a7d80a5f11d7e16
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-11 08:47:27 -0300

    Adding timestamps to animal model

--- challenge-basto-back/src/models/animal.js
+++ challenge-basto-back/src/models/animal.js
@@ -1,31 +1,34 @@
 const { Schema, model } = require("mongoose");
 
-const AnimalSchema = new Schema({
-  ID_SENASA: {
-    type: String,
-    unique: true,
-    required: [true, "The SENASA ID is required"],
-    maxLength: [16, "The SENASA ID should contain 16 characters"],
+const AnimalSchema = new Schema(
+  {
+    ID_SENASA: {
+      type: String,
+      unique: true,
+      required: [true, "The SENASA ID is required"],
+      maxLength: [16, "The SENASA ID should contain 16 characters"],
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["Novillo", "Toro", "Vaquillona"],
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    deviceType: {
+      type: String,
+      required: true,
+      enum: ["COLLAR", "CARAVANA"],
+    },
+    deviceNum: {
+      type: String,
+      required: true,
+      maxlength: 8,
+    },
   },
-  type: {
-    type: String,
-    required: true,
-    enum: ["Novillo", "Toro", "Vaquillona"],
-  },
-  weight: {
-    type: Number,
-    required: true,
-  },
-  deviceType: {
-    type: String,
-    required: true,
-    enum: ["COLLAR", "CARAVANA"],
-  },
-  deviceNum: {
-    type: String,
-    required: true,
-    maxlength: 8,
-  },
-});
+  { timestamps: { createdAt: true, updatedAt: true } }
+);
 
 module.exports = model("Animal", AnimalSchema);
