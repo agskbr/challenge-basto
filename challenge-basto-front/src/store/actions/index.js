d67ea292c0aad219c65dc36e90b818bd6af6b1d0
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-11 12:39:37 -0300

    Add alerts with sweetalert in actions.js file

--- challenge-basto-front/src/store/actions/index.js
+++ challenge-basto-front/src/store/actions/index.js
@@ -4,38 +4,85 @@ import {
   GET_ANIMAl_BY_ID,
   REQUEST_POST,
 } from "../actions-type/types";
+import swal from "sweetalert";
 
-const base_url = "http://localhost:3001";
+const base_url = "http://localhost:3001/api";
 
 const createAnimal = (animal) => {
-  return async () => {
-    await axios.post(`${base_url}/animal`, animal);
+  return async (dispatch) => {
+    try {
+      await axios.post(`${base_url}/animals`, animal);
+      swal({
+        title: "Registro creado correctamente",
+        icon: "success",
+        buttons: "Aceptar",
+      });
+      dispatch(getAllAnimals());
+    } catch (error) {
+      console.log(error);
+      swal({
+        title: `Algo salió mal ${error}`,
+        icon: "error",
+        buttons: "Aceptar",
+      });
+    }
   };
 };
 
 const getAllAnimals = () => {
   return async (dispatch) => {
-    const animals = await axios.get(`${base_url}/animal`);
-    dispatch({ type: GET_ALL_ANIMALS, payload: animals });
+    try {
+      const { data } = await axios.get(`${base_url}/animals`);
+      dispatch({ type: GET_ALL_ANIMALS, payload: data });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
 const getAnimalById = (id) => {
   return async (dispatch) => {
-    const animal = await axios.get(`${base_url}/animal/${id}`);
-    dispatch({ type: GET_ANIMAl_BY_ID, payload: animal });
+    try {
+      const { data } = await axios.get(`${base_url}/animals/${id}`);
+      dispatch({ type: GET_ANIMAl_BY_ID, payload: data });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
 const updateAnimal = (id, animal) => {
-  return async () => {
-    await axios.put(`${base_url}/animal/${id}`, animal);
+  return async (dispatch) => {
+    try {
+      await axios.put(`${base_url}/animals/${id}`, animal);
+      dispatch(getAllAnimals());
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
 const deleteAnimal = (id) => {
-  return async () => {
-    await axios.delete(`${base_url}/animal/${id}`);
+  return async (dispatch) => {
+    try {
+      const confirmDelete = await swal({
+        title: "¿Seguro quieres borrar este registro?",
+        buttons: {
+          cancel: "No estoy seguro",
+          confirm: {
+            text: "Si, quiero borrarlo",
+            value: true,
+          },
+        },
+        icon: "warning",
+      });
+      if (confirmDelete) {
+        await axios.delete(`${base_url}/animals/${id}`);
+        dispatch(getAllAnimals());
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
