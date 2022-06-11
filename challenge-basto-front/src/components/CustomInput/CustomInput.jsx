e9a6d2de0f7680585b3bde9615bcc9b9cffe8bc8
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-11 19:22:40 -0300

    with onChange function modify animals state from redux

--- challenge-basto-front/src/components/CustomInput/CustomInput.jsx
+++ challenge-basto-front/src/components/CustomInput/CustomInput.jsx
@@ -1,13 +1,24 @@
+import { findAnimalByType } from "../../store/actions";
 import CustomButton from "../CustomButton/CustomButton";
+import { useState } from "react";
 import style from "./CustomInput.module.scss";
+import { useDispatch } from "react-redux";
 
 export default function CustomInput({ titleInput, label }) {
+  const dispatch = useDispatch();
+  // const [input, setInput] = useState("");
   return (
     <div className={style.customInput}>
       <span>{titleInput}</span>
       <div className={style.inputContainer}>
-        <input type="search" placeholder={label} />
-        <CustomButton label="Buscar" />
+        <input
+          onChange={(event) => dispatch(findAnimalByType(event.target.value.toLowerCase()))}
+          type="search"
+          placeholder={label}
+        />
+        <CustomButton
+          label="Buscar"
+        />
       </div>
     </div>
   );
