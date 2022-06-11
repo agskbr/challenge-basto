be44ca5fa6b66ec648e28480d3f60440e339338a
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-11 12:41:01 -0300

    dispatch getAllAnimals action in useEffect hook

--- challenge-basto-front/src/views/AdminPage/AdminPage.jsx
+++ challenge-basto-front/src/views/AdminPage/AdminPage.jsx
@@ -1,28 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import CustomButton from "../../components/CustomButton/CustomButton";
 import CustomInput from "../../components/CustomInput/CustomInput";
-import CustomModal from "../../components/CustomModal/CustomModal";
-import CustomTable from "../../components/CustomTable/CustomTable";
+import CustomModal from "./components/CustomModal/CustomModal";
+import CustomTable from "./components/CustomTable/CustomTable";
+import { getAllAnimals } from "../../store/actions/index";
 import style from "./AdminPage.module.scss";
 import HeaderBar from "./components/HeaderBar/HeaderBar.jsx";
 
 export default function AdminPage() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllAnimals());
+  }, []);
   return (
     <div className={style.principalContainer}>
       <HeaderBar />
       <div className={style.bodyContent}>
         <div className={style.routeDynamicText}>
-          <span>Admin / Establecimiento</span>
+          <span>Admin / Animales</span>
         </div>
         <div className={style.bodyTitle}>
           <span>Establecimiento Ganadero</span>
         </div>
         <CustomButton
           onClick={() => document.getElementById("customDialog").showModal()}
-          label="Crear nuevo establecimiento"
+          label="Registrar un nuevo animal"
         />
         <CustomInput
-          titleInput="Nombre / Número de registro"
-          label="Nombre / Número de registro"
+          titleInput="Tipo animal / Número de registro"
+          label="Tipo animal / Número de registro"
         />
         <CustomModal />
         <CustomTable tableTitle="Lista de establecimientos" />
