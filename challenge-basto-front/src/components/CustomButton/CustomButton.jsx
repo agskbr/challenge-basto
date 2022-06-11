794538a404821e9212ca74bff1c40c18383732b5
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-11 12:43:37 -0300

    small changes on button and input

--- challenge-basto-front/src/components/CustomButton/CustomButton.jsx
+++ challenge-basto-front/src/components/CustomButton/CustomButton.jsx
@@ -1,11 +1,17 @@
 import style from "./CustomButton.module.scss";
 
-export default function CustomButton({ label, onClick, width = "auto" }) {
+export default function CustomButton({
+  label,
+  onClick,
+  width = "auto",
+  disabled = false,
+}) {
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
       style={{ width: `${width}` }}
-      className={style.customBtn}
+      className={disabled ? style.disabledBtn : style.customBtn}
     >
       {label}
     </button>
