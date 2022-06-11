19780ae058bdb9ecd2c0afff2fd2577d1889562a
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-11 18:25:00 -0300

    paginated finished

--- challenge-basto-front/src/views/AdminPage/components/CustomTable/CustomTable.jsx
+++ challenge-basto-front/src/views/AdminPage/components/CustomTable/CustomTable.jsx
@@ -4,13 +4,35 @@ import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { useEffect } from "react";
 import { deleteAnimal } from "../../../../store/actions";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function CustomTable({ tableTitle }) {
   const navigate = useNavigate();
   const { animals } = useSelector((state) => state);
   const dispatch = useDispatch();
   const [displayAnimals, setDisplayAnimals] = useState([]);
+  const [paginated, setPaginated] = useState({ from: 0, to: 5 });
+  const [selectedPageTab, setSelectedPageTab] = useState(1);
+
+  const calculatePaginatedTabs = () => {
+    const tabsCount = Math.ceil(displayAnimals.length / 5);
+    const tabs = [];
+    for (let i = 1; i <= tabsCount; i++) {
+      tabs.push(i);
+    }
+    return tabs;
+  };
+
+  const isDisabledPaginatedButton = () => {
+    const arrLength = displayAnimals.slice(
+      paginated.from + 5,
+      paginated.to + 5
+    ).length;
+    if (!arrLength) {
+      return true;
+    }
+    return false;
+  };
 
   useEffect(() => {
     if (animals) {
@@ -36,7 +58,7 @@ export default function CustomTable({ tableTitle }) {
           </tr>
         </thead>
         <tbody>
-          {displayAnimals.map((animal) => (
+          {displayAnimals.slice(paginated.from, paginated.to).map((animal) => (
             <tr key={animal._id}>
               <td>{animal.ID_SENASA}</td>
               <td>{animal.type}</td>
@@ -66,11 +88,50 @@ export default function CustomTable({ tableTitle }) {
         </tbody>
       </table>
       <div className={style.paginated}>
-        <div className={style.disabledPaginatedBtn}>Prev</div>
-        <div className={style.activePage}>1</div>
-        <div className={style.page}>2</div>
-        <div className={style.page}>3</div>
-        <div className={style.nextBtn}>Next</div>
+        <div
+          className={
+            paginated.from < 5 ? style.disabledPaginatedBtn : style.prevBtn
+          }
+          onClick={() => {
+            if (paginated.from < 5) {
+              return;
+            }
+            setSelectedPageTab((state) => state - 1);
+            setPaginated((state) => ({
+              from: state.from - 5,
+              to: state.to - 5,
+            }));
+          }}
+        >
+          Prev
+        </div>
+        {/* <div className={style.activePage}>1</div> */}
+        {calculatePaginatedTabs(displayAnimals).map((tab) => (
+          <div
+            key={tab}
+            className={selectedPageTab === tab ? style.activePage : style.page}
+          >
+            {tab}
+          </div>
+        ))}
+        <div
+          className={
+            isDisabledPaginatedButton()
+              ? style.disabledPaginatedBtn
+              : style.nextBtn
+          }
+          onClick={() => {
+            if (!isDisabledPaginatedButton()) {
+              setSelectedPageTab((state) => state + 1);
+              setPaginated((state) => ({
+                from: state.from + 5,
+                to: state.to + 5,
+              }));
+            }
+          }}
+        >
+          Next
+        </div>
       </div>
     </div>
   );
