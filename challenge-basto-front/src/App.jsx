7bb29db375d0aec149744e1c18da7f8f68923afe
Agustín Cabrera <69356711+agskbr@users.noreply.github.com>
2022-06-11 12:28:34 -0300

    Add route to edit page

--- challenge-basto-front/src/App.jsx
+++ challenge-basto-front/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import AdminPage from "./views/AdminPage/AdminPage";
 import { Routes, Route } from "react-router-dom";
+import EditPage from "./views/EditPage/EditPage";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="*" element={<div>Not found page</div>} />
         <Route path="/" element={<AdminPage />} />
+        <Route path="/edit/:id" element={<EditPage />} />
       </Routes>
     </div>
   );
